9f20301b481f01bc3e2701ad79c3f2710f36de31
Josh Nichols <technicalpickles@github.com>
2014-12-07 16:37:27 -0500

    Include behavior spec

--- spec/lita/handlers/shipit_spec.rb
+++ spec/lita/handlers/shipit_spec.rb
@@ -5,4 +5,9 @@ describe Lita::Handlers::Shipit, lita_handler: true do
   it { is_expected.to route("shipit").to(:shipit) }
   it { is_expected.to route("ship it").to(:shipit) }
   it { is_expected.to route("YOU SHOULD PROBABLY JUST SHIP IT NOW").to(:shipit) }
+
+  it "ships a shipit squirrel" do
+    send_message("le ship it")
+    expect(Lita::Handlers::Shipit::SQUIRRELS).to include(replies.last)
+  end
 end
